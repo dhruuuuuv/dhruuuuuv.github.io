dadaffadbb35cdb298b006634e2620157c3f2b79
dhruv chauhan <dc14690@my.bristol.ac.uk>
2019-08-07 22:48:51 +0100

    ADD: random details

--- js/index.js
+++ js/index.js
@@ -1,5 +1,24 @@
-function toggleDiv(id) {
-    var div = document.getElementById(id);
-    div.style.display = div.style.display == "none" ? "block" : "none";
-  return false;
+// function toggleDiv (id) {
+//   var div = document.getElementById(id);
+//   div.style.display = div.style.display == "none" ? "block" : "none";
+//   return false;
+// }
+
+var target = document.getElementById('target');
+var personas = [
+  'plant lover',
+  'fishbowl gazer',
+  'amateur watercolourist',
+  'fresh air enthusiast',
+  'bleep maker',
+  'dog devotee',
+  'chilli addict',
+  'humble hacker'
+];
+
+function newPersona () {
+  var i = (Math.random() * personas.length) | 0;
+  target.innerText = personas[i];
 }
+
+newPersona();
